f69c31f78047cb977f3b82b7d69561fbbf2eaf6c
MinGyu Chang <mgchang02@gmail.com>
2026-05-19 21:09:26 +0900

    scripts

--- scripts/run_datasets.sh
+++ scripts/run_datasets.sh
@@ -2,23 +2,23 @@
 
 GPU_ID=0
 
-echo "=== [0] LIFT+ baseline ==="
+# echo "=== [0] LIFT+ baseline ==="
 
-CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py \
-    -d places_lt -b clip_vit_b16 -m lift+ \
-    classifier_init semantic \
-    tte True \
-    TEXT_REG_LAMBDA 0.0 \
-    INFONCE_LAMBDA 0.0 \
-    output_dir baseline/places_lt
+# CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py \
+#     -d places_lt -b clip_vit_b16 -m lift+ \
+#     classifier_init semantic \
+#     tte True \
+#     TEXT_REG_LAMBDA 0.0 \
+#     INFONCE_LAMBDA 0.0 \
+#     output_dir baseline/places_lt
 
-CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py \
-    -d inat2018 -b clip_vit_b16 -m lift+ num_epochs 15 \
-    classifier_init semantic \
-    tte True \
-    TEXT_REG_LAMBDA 0.0 \
-    INFONCE_LAMBDA 0.0 \
-    output_dir baseline/inat2018
+# CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py \
+#     -d inat2018 -b clip_vit_b16 -m lift+ num_epochs 15 \
+#     classifier_init semantic \
+#     tte True \
+#     TEXT_REG_LAMBDA 0.0 \
+#     INFONCE_LAMBDA 0.0 \
+#     output_dir baseline/inat2018
 
 
 echo "=== [1] Proposed Method ==="
@@ -26,11 +26,22 @@ echo "=== [1] Proposed Method ==="
 CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py \
     -d places_lt -b clip_vit_b16 -m lift+ \
     tte True \
+    INFONCE_LAMBDA 0.0 \
     PEFT_WARMUP True \
     output_dir final_tte/places_lt
 
 CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py \
     -d inat2018 -b clip_vit_b16 -m lift+ num_epochs 15 \
     tte True \
+    SIM_THRESHOLD 0.8 \
     PEFT_WARMUP True \
     output_dir final_tte/inat2018
+
+CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py \
+    -d inat2018 -b clip_vit_b16 -m lift+ num_epochs 15 \
+    tte True \
+    SIM_THRESHOLD 0.8 \
+    INFONCE_LAMBDA 0.0 \
+    PEFT_WARMUP True \
+    output_dir final_tte/inat2018
+
