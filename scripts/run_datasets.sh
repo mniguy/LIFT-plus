197a7268df76389d4a659fb7feae5b611f986254
MinGyu Chang <mgchang02@gmail.com>
2026-05-19 13:55:13 +0900

    fix errors

--- scripts/run_datasets.sh
+++ scripts/run_datasets.sh
@@ -7,15 +7,15 @@ echo "=== [0] LIFT+ baseline ==="
 CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py \
     -d places_lt -b clip_vit_b16 -m lift+ \
     classifier_init semantic \
-    TEXT_REG_LAMBDA 0 \
-    INFONCE_LAMBDA 0 \
+    TEXT_REG_LAMBDA 0.0 \
+    INFONCE_LAMBDA 0.0 \
     output_dir baseline/places_lt
 
 CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py \
     -d inat2018 -b clip_vit_b16 -m lift+ num_epochs 15 \
     classifier_init semantic \
-    TEXT_REG_LAMBDA 0 \
-    INFONCE_LAMBDA 0 \
+    TEXT_REG_LAMBDA 0.0 \
+    INFONCE_LAMBDA 0.0 \
     output_dir baseline/inat2018
 
 
